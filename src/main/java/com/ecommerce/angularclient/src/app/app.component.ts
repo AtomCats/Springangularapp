7de2302d767294eaf1f7ef6401d1ec2a5a1d1687
Anton Eremin <aeremin@wiley.com>
2019-12-19 19:44:27 +0300

    Added filter sidebar

--- src/main/java/com/ecommerce/angularclient/src/app/app.component.ts
+++ src/main/java/com/ecommerce/angularclient/src/app/app.component.ts
@@ -7,28 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularclient';
-  acc:HTMLElement;
+  acc: HTMLElement;
 
   constructor() {
     this.title = 'Admin tool';
   }
 
   openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById('sidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
   }
 
-  closeNav(){
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  closeNav() {
+    document.getElementById('sidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+  openFilter() {
+    document.getElementById('sidefilter').style.width = '250px';
   }
 
-  collapse(classname:string) {
+  closeFilter() {
+    document.getElementById('sidefilter').style.width = '0';
+  }
+
+  collapse(classname: string) {
     this.acc = document.getElementById(classname);
-    if (this.acc.style.display === "block") {
-      this.acc.style.display = "none";
+    if (this.acc.style.display === 'block') {
+      this.acc.style.display = 'none';
     } else {
-      this.acc.style.display = "block";
+      this.acc.style.display = 'block';
     }
   }
 
